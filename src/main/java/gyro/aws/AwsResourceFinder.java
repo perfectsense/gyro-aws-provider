8e4bbd9430a5936935f10f1011b5d155c50fbbae
Hao Sun <PSDhao@users.noreply.github.com>
2019-03-11 14:49:31 -0400

    Turn ResourceFinder into an interface

--- src/main/java/gyro/aws/AwsResourceFinder.java
+++ src/main/java/gyro/aws/AwsResourceFinder.java
@@ -2,25 +2,23 @@ package gyro.aws;
 
 import com.psddev.dari.util.TypeDefinition;
 import gyro.core.BeamException;
-import gyro.core.query.QueryField;
-import gyro.core.query.QueryType;
+import gyro.lang.Credentials;
 import gyro.lang.ResourceFinder;
 import software.amazon.awssdk.core.SdkClient;
 import software.amazon.awssdk.services.ec2.model.Filter;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public abstract class AwsResourceFinder<C extends SdkClient, A, R extends AwsResource> extends ResourceFinder<R> {
+public abstract class AwsResourceFinder<C extends SdkClient, A, R extends AwsResource> implements ResourceFinder<R> {
 
     private SdkClient client;
 
-    protected <T extends SdkClient> T createClient(Class<T> clientClass) {
+    protected <T extends SdkClient> T createClient(Class<T> clientClass, Credentials credentials) {
         if (client == null) {
-            client = AwsResource.createClient(clientClass, (AwsCredentials) credentials());
+            client = AwsResource.createClient(clientClass, (AwsCredentials) credentials);
         }
 
         return (T) client;
@@ -32,41 +30,22 @@ public abstract class AwsResourceFinder<C extends SdkClient, A, R extends AwsRes
             .collect(Collectors.toList());
     }
 
-    public final List<R> query() {
-        Map<String, String> filters = new HashMap<>();
-        for (QueryField field : QueryType.getInstance(getClass()).getFields()) {
-            String filterName = field.getFilterName();
-            Object value = field.getValue(this);
-            if (value instanceof Map) {
-                Map<?, ?> valueMap = (Map<?, ?>) value;
-                for (Map.Entry<?, ?> entry : valueMap.entrySet()) {
-                    filters.put(String.format("%s:%s", filterName, entry.getKey()), entry.getValue().toString());
-                }
-
-            } else if (value != null) {
-                filters.put(filterName, value.toString());
-            }
-        }
-
-        return filters.isEmpty() ? findAll() : find(filters);
-    }
-
     protected abstract List<A> findAws(C client, Map<String, String> filters);
 
     @Override
-    public final List<R> find(Map<String, String> filters) {
+    public final List<R> find(Credentials credentials, Map<String, String> filters) {
         TypeDefinition td = TypeDefinition.getInstance(getClass());
         Class<C> clientClass = td.getInferredGenericTypeArgumentClass(AwsResourceFinder.class, 0);
-        return findAws(createClient(clientClass), filters).stream().map(this::createResource).collect(Collectors.toList());
+        return findAws(createClient(clientClass, credentials), filters).stream().map(this::createResource).collect(Collectors.toList());
     }
 
     protected abstract List<A> findAllAws(C client);
 
     @Override
-    public final List<R> findAll() {
+    public final List<R> findAll(Credentials credentials) {
         TypeDefinition td = TypeDefinition.getInstance(getClass());
         Class<C> clientClass = td.getInferredGenericTypeArgumentClass(AwsResourceFinder.class, 0);
-        return findAllAws(createClient(clientClass)).stream().map(this::createResource).collect(Collectors.toList());
+        return findAllAws(createClient(clientClass, credentials)).stream().map(this::createResource).collect(Collectors.toList());
     }
 
     @SuppressWarnings("unchecked")
@@ -77,7 +56,7 @@ public abstract class AwsResourceFinder<C extends SdkClient, A, R extends AwsRes
         Class<R> resourceClass = td.getInferredGenericTypeArgumentClass(AwsResourceFinder.class, 2);
 
         try {
-            return resourceClass.getConstructor(clientClass, modelClass).newInstance(createClient(clientClass), model);
+            return resourceClass.getConstructor(clientClass, modelClass).newInstance(client, model);
         } catch (NoSuchMethodException nme) {
             throw new BeamException(String.format("No constructor %s(%s, %s) is defined!", resourceClass, clientClass, modelClass));
         } catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
