d04c07966781c3094e9ff1880bde187d93df8365
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-05-01 20:14:48 -0400

    add security group field.

--- src/main/java/gyro/aws/elasticache/CacheSecurityGroupResource.java
+++ src/main/java/gyro/aws/elasticache/CacheSecurityGroupResource.java
@@ -1,18 +1,32 @@
 package gyro.aws.elasticache;
 
+import com.google.common.collect.MapDifference;
+import com.google.common.collect.Maps;
 import com.psddev.dari.util.ObjectUtils;
 import gyro.aws.AwsResource;
+import gyro.core.GyroException;
 import gyro.core.resource.Resource;
+import gyro.core.resource.ResourceDiffProperty;
+import gyro.core.resource.ResourceName;
+import gyro.core.resource.ResourceOutput;
 import software.amazon.awssdk.services.elasticache.ElastiCacheClient;
 import software.amazon.awssdk.services.elasticache.model.CacheSecurityGroup;
 import software.amazon.awssdk.services.elasticache.model.CreateCacheSecurityGroupResponse;
 import software.amazon.awssdk.services.elasticache.model.DescribeCacheSecurityGroupsResponse;
+import software.amazon.awssdk.services.elasticache.model.EC2SecurityGroup;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+@ResourceName("cache-security-group")
 public class CacheSecurityGroupResource extends AwsResource {
     private String cacheSecurityGroupName;
     private String description;
+    private Map<String, String> ec2SecurityGroups;
+
+    private String ownerId;
 
     public String getCacheSecurityGroupName() {
         return cacheSecurityGroupName;
@@ -30,6 +44,28 @@ public class CacheSecurityGroupResource extends AwsResource {
         this.description = description;
     }
 
+    @ResourceDiffProperty(updatable = true)
+    public Map<String, String> getEc2SecurityGroups() {
+        if (ec2SecurityGroups == null) {
+            ec2SecurityGroups = new HashMap<>();
+        }
+
+        return ec2SecurityGroups;
+    }
+
+    public void setEc2SecurityGroups(Map<String, String> ec2SecurityGroups) {
+        this.ec2SecurityGroups = ec2SecurityGroups;
+    }
+
+    @ResourceOutput
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
     @Override
     public boolean refresh() {
         ElastiCacheClient client = createClient(ElastiCacheClient.class);
@@ -42,7 +78,13 @@ public class CacheSecurityGroupResource extends AwsResource {
             CacheSecurityGroup cacheSecurityGroup = response.cacheSecurityGroups().get(0);
 
             setDescription(cacheSecurityGroup.description());
-            cacheSecurityGroup.ec2SecurityGroups();
+            setOwnerId(cacheSecurityGroup.ownerId());
+
+            getEc2SecurityGroups().clear();
+            setEc2SecurityGroups(cacheSecurityGroup.ec2SecurityGroups().stream()
+                .collect(
+                    Collectors.toMap(EC2SecurityGroup::ec2SecurityGroupName, EC2SecurityGroup::ec2SecurityGroupOwnerId)
+                ));
 
             return true;
         } else {
@@ -58,15 +100,69 @@ public class CacheSecurityGroupResource extends AwsResource {
             r -> r.description(getDescription())
                 .cacheSecurityGroupName(getCacheSecurityGroupName())
         );
+
+        setOwnerId(response.cacheSecurityGroup().ownerId());
+
+        try {
+            if (!getEc2SecurityGroups().isEmpty()) {
+                for (String key : getEc2SecurityGroups().keySet()) {
+                    client.authorizeCacheSecurityGroupIngress(
+                        r -> r.cacheSecurityGroupName(getCacheSecurityGroupName())
+                            .ec2SecurityGroupName(key)
+                            .ec2SecurityGroupOwnerId(getEc2SecurityGroups().get(key))
+                    );
+                }
+            }
+        } catch (Exception ex) {
+            throw new GyroException(String.format("Error adding ec2 security groups to cache security group %s",
+                getCacheSecurityGroupName()), ex);
+        }
     }
 
     @Override
     public void update(Resource current, Set<String> changedProperties) {
         ElastiCacheClient client = createClient(ElastiCacheClient.class);
 
-        client.authorizeCacheSecurityGroupIngress(
-            r -> r.cacheSecurityGroupName(getCacheSecurityGroupName())
-        );
+        Map<String, String> currentEc2SecurityGroups = ((CacheSecurityGroupResource) current).getEc2SecurityGroups();
+
+        MapDifference<String, String> diff = Maps.difference(currentEc2SecurityGroups, getEc2SecurityGroups());
+
+        if (!diff.entriesOnlyOnLeft().isEmpty()) {
+            for (String key: diff.entriesOnlyOnLeft().keySet()) {
+                client.revokeCacheSecurityGroupIngress(
+                    r -> r.cacheSecurityGroupName(getCacheSecurityGroupName())
+                        .ec2SecurityGroupName(key)
+                        .ec2SecurityGroupOwnerId(diff.entriesOnlyOnLeft().get(key))
+                );
+            }
+        }
+
+        if (!diff.entriesOnlyOnRight().isEmpty()) {
+            for (String key : diff.entriesOnlyOnRight().keySet())
+            client.authorizeCacheSecurityGroupIngress(
+                r -> r.cacheSecurityGroupName(getCacheSecurityGroupName())
+                    .ec2SecurityGroupName(key)
+                    .ec2SecurityGroupOwnerId(diff.entriesOnlyOnRight().get(key))
+            );
+        }
+
+        if (!diff.entriesDiffering().isEmpty()) {
+            for (String key: diff.entriesDiffering().keySet()) {
+                client.revokeCacheSecurityGroupIngress(
+                    r -> r.cacheSecurityGroupName(getCacheSecurityGroupName())
+                        .ec2SecurityGroupName(key)
+                        .ec2SecurityGroupOwnerId(diff.entriesDiffering().get(key).leftValue())
+                );
+            }
+
+            for (String key: diff.entriesDiffering().keySet()) {
+                client.authorizeCacheSecurityGroupIngress(
+                    r -> r.cacheSecurityGroupName(getCacheSecurityGroupName())
+                        .ec2SecurityGroupName(key)
+                        .ec2SecurityGroupOwnerId(diff.entriesDiffering().get(key).rightValue())
+                );
+            }
+        }
     }
 
     @Override
