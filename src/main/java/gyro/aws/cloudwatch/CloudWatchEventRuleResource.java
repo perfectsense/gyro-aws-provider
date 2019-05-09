f9100f15b8fe7be84eb64092cbf774bea3634f6e
Vishakha Sadhwani <vishakha@perfectsensedigital.com>
2019-05-09 11:13:35 -0400

    Add documentation for rule resource and clean code.

--- src/main/java/gyro/aws/cloudwatch/CloudWatchEventRuleResource.java
+++ src/main/java/gyro/aws/cloudwatch/CloudWatchEventRuleResource.java
@@ -15,7 +15,11 @@ import software.amazon.awssdk.services.cloudwatchevents.model.PutRuleResponse;
 import software.amazon.awssdk.services.cloudwatchevents.model.Rule;
 import software.amazon.awssdk.services.cloudwatchevents.model.Target;
 
+import software.amazon.awssdk.utils.IoUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -23,16 +27,124 @@ import java.util.Set;
 @ResourceType("event-rule")
 public class CloudWatchEventRuleResource extends AwsResource {
 
-    private String ruleName;
     private String description;
     private String eventPattern;
-    private String roleArn;
-    private String scheduleExpression;
-    private String state;
-    private String ruleArn;
+    private String eventPatternPath;
     private String managedBy;
+    private String roleArn;
+    private String ruleArn;
+    private String ruleName;
+    private String scheduleExpression;
+    private Boolean scheduleEvent;
+    private String state;
     private List<CloudWatchRuleTargetResource> target;
 
+    /**
+     * The name of the rule associated that matches incoming events. (Required)
+     */
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
+
+    /**
+     * The description of the rule associated with the event.(Optional)
+     */
+    @ResourceUpdatable
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * The iam role arn that gives permissions to invoke actions on the targets.
+     */
+    @ResourceUpdatable
+    public String getRoleArn() {
+        return roleArn;
+    }
+
+    public void setRoleArn(String roleArn) {
+        this.roleArn = roleArn;
+    }
+
+    /**
+     * The Boolean value that indicates if the rule is a scheduled type.
+     */
+    public Boolean getScheduleEvent() {
+        if (scheduleEvent == null) {
+            scheduleEvent = false;
+        }
+        return scheduleEvent;
+    }
+
+    public void setScheduleEvent(Boolean scheduleEvent) {
+        this.scheduleEvent = scheduleEvent;
+    }
+
+    /**
+     * The event pattern which activates target actions when matched.
+     */
+    @ResourceUpdatable
+    public String getEventPattern() {
+        return eventPattern;
+    }
+
+    public void setEventPattern(String eventPattern) {
+        this.eventPattern = eventPattern;
+    }
+
+    /**
+     * The location to the json file which contains the event pattern.
+     */
+    @ResourceUpdatable
+    public String getEventPatternPath() {
+        return eventPatternPath;
+    }
+
+    public void setEventPatternPath(String eventPatternPath) {
+        this.eventPatternPath = eventPatternPath;
+    }
+
+    /**
+     * The schedule expression that triggers an automated rule action.
+     *
+     * See `AWS Services Schedule Expressions <https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html/>`_.
+     */
+    @ResourceUpdatable
+    public String getScheduleExpression() {
+        return scheduleExpression;
+    }
+
+    public void setScheduleExpression(String scheduleExpression) {
+        this.scheduleExpression = scheduleExpression;
+    }
+
+    /**
+     * This value indicates if the rule is enabled to invoke target actions.
+     *
+     * Valid values are ``ENABLED`` and ``DISABLED``
+     */
+    @ResourceUpdatable
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * The list of target resources which can be invoked when an event pattern is matched or scheduled
+     *
+     * See `AWS Services CloudWatch events <https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/WhatIsCloudWatchEvents.html/>`_.
+     */
     @ResourceUpdatable
     public List<CloudWatchRuleTargetResource> getTarget() {
         if (target == null) {
@@ -45,67 +157,9 @@ public class CloudWatchEventRuleResource extends AwsResource {
         this.target = target;
     }
 
-    public String getRuleArn() {
-        return ruleArn;
-    }
-
-    public void setRuleArn(String ruleArn) {
-        this.ruleArn = ruleArn;
-    }
-
-    public String getRuleName() {
-        return ruleName;
-    }
-
-    public void setRuleName(String ruleName) {
-        this.ruleName = ruleName;
-    }
-
-    @ResourceUpdatable
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @ResourceUpdatable
-    public String getEventPattern() {
-        return eventPattern;
-    }
-
-    public void setEventPattern(String eventPattern) {
-        this.eventPattern = eventPattern;
-    }
-
-    @ResourceUpdatable
-    public String getRoleArn() {
-        return roleArn;
-    }
-
-    public void setRoleArn(String roleArn) {
-        this.roleArn = roleArn;
-    }
-
-    @ResourceUpdatable
-    public String getScheduleExpression() {
-        return scheduleExpression;
-    }
-
-    public void setScheduleExpression(String scheduleExpression) {
-        this.scheduleExpression = scheduleExpression;
-    }
-
-    @ResourceUpdatable
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
+    /**
+     * If role arn is given, this field specifies which aws service can manage the rules.
+     */
     @ResourceUpdatable
     public String getManagedBy() {
         return managedBy;
@@ -113,6 +167,14 @@ public class CloudWatchEventRuleResource extends AwsResource {
 
     public void setManagedBy(String managedBy) {
         this.managedBy = managedBy;
+    }
+
+    public String getRuleArn() {
+        return ruleArn;
+    }
+
+    public void setRuleArn(String ruleArn) {
+        this.ruleArn = ruleArn;
     }
 
     @Override
@@ -176,18 +238,6 @@ public class CloudWatchEventRuleResource extends AwsResource {
         return sb.toString();
     }
 
-    private void saveRule(CloudWatchEventsClient client) {
-        PutRuleResponse ruleResponse = client.putRule(
-            r -> r.name(getRuleName())
-            .description(getDescription())
-            .scheduleExpression(getScheduleExpression())
-            .eventPattern(getEventPattern())
-            .roleArn(getRoleArn())
-            .state(getState())
-        );
-        setRuleArn(ruleResponse.ruleArn());
-    }
-
     private Rule getRule(CloudWatchEventsClient client) {
 
         if (ObjectUtils.isBlank(getRuleName())) {
@@ -212,12 +262,64 @@ public class CloudWatchEventRuleResource extends AwsResource {
         }
     }
 
+    private void loadRule(Rule rule) {
+        setRuleArn(rule.arn());
+        setRuleName(rule.name());
+        setDescription(rule.description());
+        setEventPattern(rule.eventPattern());
+        setScheduleExpression(rule.scheduleExpression());
+        setState(rule.state().toString());
+        setRoleArn(rule.roleArn());
+        setManagedBy(rule.managedBy());
+
+        if (rule.scheduleExpression() != null) {
+            setScheduleEvent(true);
+        }
+    }
+
+    private void saveRule(CloudWatchEventsClient client) {
+
+        if (getScheduleEvent()) {
+            PutRuleResponse ruleResponse = client.putRule(
+                    r -> r.name(getRuleName())
+                            .description(getDescription())
+                            .scheduleExpression(getScheduleExpression())
+                            .roleArn(getRoleArn())
+                            .state(getState())
+            );
+            setRuleArn(ruleResponse.ruleArn());
+        } else {
+
+            if (!getEventPatternPath().isEmpty()) {
+                setEventPatternFromPath();
+            }
+
+            PutRuleResponse ruleResponse = client.putRule(
+                    r -> r.name(getRuleName())
+                            .description(getDescription())
+                            .eventPattern(getEventPattern())
+                            .roleArn(getRoleArn())
+                            .state(getState())
+            );
+            setRuleArn(ruleResponse.ruleArn());
+        }
+    }
+
+    private void setEventPatternFromPath() {
+        try (InputStream input = openInput(getEventPatternPath())) {
+            setEventPattern(IoUtils.toUtf8String(input));
+
+        } catch (IOException ioex) {
+            throw new GyroException(MessageFormat
+                    .format("Event Pattern - {0} file error. Unable to read event pattern from path [{1}]", getRuleName(), getEventPatternPath()));
+        }
+    }
+
     private List<Target> getTarget(CloudWatchEventsClient client, Rule rule) {
 
         if (getTarget().isEmpty()) {
             throw new GyroException("No targets presents, unable to load rule target.");
         }
-
 
         ListTargetsByRuleResponse listTargetResponse = client.listTargetsByRule(r -> r.rule(rule.name()));
 
@@ -235,16 +337,5 @@ public class CloudWatchEventRuleResource extends AwsResource {
 
             throw ex;
         }
-    }
-
-    private void loadRule(Rule rule) {
-        setRuleArn(rule.arn());
-        setRuleName(rule.name());
-        setDescription(rule.description());
-        setEventPattern(rule.eventPattern());
-        setScheduleExpression(rule.scheduleExpression());
-        setState(rule.state().toString());
-        setRoleArn(rule.roleArn());
-        setManagedBy(rule.managedBy());
     }
 }
