cbdc066dd551cc875a7ef2b3c140d71a43d6894c
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-05-17 10:10:11 -0400

    Add vpn gateway resource finder.

---
+++ src/main/java/gyro/aws/ec2/VpnGatewayResourceFinder.java
@@ -0,0 +1,112 @@
+package gyro.aws.ec2;
+
+import gyro.aws.AwsResourceFinder;
+import gyro.core.resource.ResourceFilter;
+import gyro.core.resource.ResourceType;
+import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.model.VpnGateway;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ResourceType("vpn-gateway")
+public class VpnGatewayResourceFinder extends AwsResourceFinder<Ec2Client, VpnGateway, VpnGatewayResource> {
+    private String amazonSideAsn;
+    private String attachmentState;
+    private String attachmentVpcId;
+    private String availabilityZone;
+    private String state;
+    private Map<String, String> tag;
+    private String tagKey;
+    private String type;
+    private String vpnGatewayId;
+
+    public String getAmazonSideAsn() {
+        return amazonSideAsn;
+    }
+
+    public void setAmazonSideAsn(String amazonSideAsn) {
+        this.amazonSideAsn = amazonSideAsn;
+    }
+
+    @ResourceFilter("attachment.state")
+    public String getAttachmentState() {
+        return attachmentState;
+    }
+
+    public void setAttachmentState(String attachmentState) {
+        this.attachmentState = attachmentState;
+    }
+
+    @ResourceFilter("attachment.vpc-id")
+    public String getAttachmentVpcId() {
+        return attachmentVpcId;
+    }
+
+    public void setAttachmentVpcId(String attachmentVpcId) {
+        this.attachmentVpcId = attachmentVpcId;
+    }
+
+    public String getAvailabilityZone() {
+        return availabilityZone;
+    }
+
+    public void setAvailabilityZone(String availabilityZone) {
+        this.availabilityZone = availabilityZone;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Map<String, String> getTag() {
+        if (tag == null) {
+            tag = new HashMap<>();
+        }
+
+        return tag;
+    }
+
+    public void setTag(Map<String, String> tag) {
+        this.tag = tag;
+    }
+
+    public String getTagKey() {
+        return tagKey;
+    }
+
+    public void setTagKey(String tagKey) {
+        this.tagKey = tagKey;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getVpnGatewayId() {
+        return vpnGatewayId;
+    }
+
+    public void setVpnGatewayId(String vpnGatewayId) {
+        this.vpnGatewayId = vpnGatewayId;
+    }
+
+    @Override
+    protected List<VpnGateway> findAws(Ec2Client client, Map<String, String> filters) {
+        return client.describeVpnGateways(r -> r.filters(createFilters(filters))).vpnGateways();
+    }
+
+    @Override
+    protected List<VpnGateway> findAllAws(Ec2Client client) {
+        return client.describeVpnGateways().vpnGateways();
+    }
+}
