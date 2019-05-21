f3d3add52073689fe235e8471fdae5d5f4dfdc07
Hyoo Lim <hyoolim@gmail.com>
2019-05-21 11:45:17 -0400

    Adds Copyable for copying underlying AWS model data into Gyro resources.

--- src/main/java/gyro/aws/AwsFinder.java
+++ src/main/java/gyro/aws/AwsFinder.java
@@ -1,66 +1,58 @@
 package gyro.aws;
 
 import com.psddev.dari.util.TypeDefinition;
-import gyro.core.GyroException;
-import gyro.core.auth.Credentials;
 import gyro.core.finder.Finder;
 import software.amazon.awssdk.core.SdkClient;
 import software.amazon.awssdk.services.ec2.model.Filter;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public abstract class AwsFinder<C extends SdkClient, A, R extends AwsResource> extends Finder<R> {
+public abstract class AwsFinder<C extends SdkClient, M, R extends AwsResource> extends Finder<R> {
 
-    private SdkClient client;
+    protected abstract List<M> findAllAws(C client);
 
-    protected <T extends SdkClient> T createClient(Class<T> clientClass, Credentials credentials) {
-        if (client == null) {
-            client = AwsResource.createClient(clientClass, (AwsCredentials) credentials);
-        }
-
-        return (T) client;
+    @Override
+    public final List<R> findAll() {
+        return findAllAws(newClient()).stream()
+            .map(this::newResource)
+            .collect(Collectors.toList());
     }
 
-    protected List<Filter> createFilters(Map<String, String> query) {
+    private C newClient() {
+        @SuppressWarnings("unchecked")
+        Class<C> clientClass = (Class<C>) TypeDefinition.getInstance(getClass())
+            .getInferredGenericTypeArgumentClass(AwsFinder.class, 0);
+
+        return AwsResource.createClient(clientClass, (AwsCredentials) credentials());
+    }
+
+    @SuppressWarnings("unchecked")
+    private R newResource(M model) {
+        R resource = newResource();
+
+        if (resource instanceof Copyable) {
+            ((Copyable<M>) resource).copyFrom(model);
+        }
+
+        return resource;
+    }
+
+
+    protected abstract List<M> findAws(C client, Map<String, String> filters);
+
+    @Override
+    public final List<R> find(Map<String, String> filters) {
+        return findAws(newClient(), filters).stream()
+            .map(this::newResource)
+            .collect(Collectors.toList());
+    }
+
+    public List<Filter> createFilters(Map<String, String> query) {
         return query.entrySet().stream()
             .map(e -> Filter.builder().name(e.getKey()).values(e.getValue()).build())
             .collect(Collectors.toList());
     }
 
-    protected abstract List<A> findAws(C client, Map<String, String> filters);
-
-    @Override
-    public final List<R> find(Credentials credentials, Map<String, String> filters) {
-        TypeDefinition td = TypeDefinition.getInstance(getClass());
-        Class<C> clientClass = td.getInferredGenericTypeArgumentClass(AwsFinder.class, 0);
-        return findAws(createClient(clientClass, credentials), filters).stream().map(this::createResource).collect(Collectors.toList());
-    }
-
-    protected abstract List<A> findAllAws(C client);
-
-    @Override
-    public final List<R> findAll(Credentials credentials) {
-        TypeDefinition td = TypeDefinition.getInstance(getClass());
-        Class<C> clientClass = td.getInferredGenericTypeArgumentClass(AwsFinder.class, 0);
-        return findAllAws(createClient(clientClass, credentials)).stream().map(this::createResource).collect(Collectors.toList());
-    }
-
-    @SuppressWarnings("unchecked")
-    private R createResource(A model) {
-        TypeDefinition td = TypeDefinition.getInstance(getClass());
-        Class<C> clientClass = td.getInferredGenericTypeArgumentClass(AwsFinder.class, 0);
-        Class<A> modelClass = td.getInferredGenericTypeArgumentClass(AwsFinder.class, 1);
-        Class<R> resourceClass = td.getInferredGenericTypeArgumentClass(AwsFinder.class, 2);
-
-        try {
-            return resourceClass.getConstructor(clientClass, modelClass).newInstance(client, model);
-        } catch (NoSuchMethodException nme) {
-            throw new GyroException(String.format("No constructor %s(%s, %s) is defined!", resourceClass, clientClass, modelClass));
-        } catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
-            throw new GyroException(String.format("Unable to create resource of [%s]", resourceClass), e);
-        }
-    }
 }
