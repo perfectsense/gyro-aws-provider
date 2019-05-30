90a45a56b4422d978098d95fd6afeb23d1ac18b9
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-05-30 18:42:07 -0400

    Geo Match condition and filter refactor.

--- src/main/java/gyro/aws/waf/common/GeoMatchSetResource.java
+++ src/main/java/gyro/aws/waf/common/GeoMatchSetResource.java
@@ -1,6 +1,9 @@
 package gyro.aws.waf.common;
 
-public abstract class GeoMatchSetResource extends ConditionResource {
+import gyro.aws.Copyable;
+import software.amazon.awssdk.services.waf.model.GeoMatchSet;
+
+public abstract class GeoMatchSetResource extends ConditionResource implements Copyable<GeoMatchSet> {
     @Override
     String getDisplayName() {
         return "geo match set";
