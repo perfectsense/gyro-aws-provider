d86c3e3356f822bbd8dbb9e3db3be76e3b54139b
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-06-03 12:07:49 -0400

    Add route table finder doc.

--- src/main/java/gyro/aws/ec2/RouteTableFinder.java
+++ src/main/java/gyro/aws/ec2/RouteTableFinder.java
@@ -6,11 +6,20 @@ import gyro.core.finder.Filter;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 import software.amazon.awssdk.services.ec2.model.RouteTable;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Query route table.
+ *
+ * .. code-block:: gyro
+ *
+ *    route-table: $(aws::route-table EXTERNAL/* | route-table-id = '')
+ */
 @Type("route-table")
 public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTableResource> {
+
     private String associationRouteTableAssociationId;
     private String associationRouteTableId;
     private String associationSubnetId;
@@ -33,6 +42,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
     private String transitGatewayId;
     private String vpcId;
 
+    /**
+     * The ID of an association ID for the route table.
+     */
     @Filter("association.route-table-association-id")
     public String getAssociationRouteTableAssociationId() {
         return associationRouteTableAssociationId;
@@ -42,6 +54,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.associationRouteTableAssociationId = associationRouteTableAssociationId;
     }
 
+    /**
+     * The ID of the route table involved in the association.
+     */
     @Filter("association.route-table-id")
     public String getAssociationRouteTableId() {
         return associationRouteTableId;
@@ -51,6 +66,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.associationRouteTableId = associationRouteTableId;
     }
 
+    /**
+     * The ID of the subnet involved in the association.
+     */
     @Filter("association.subnet-id")
     public String getAssociationSubnetId() {
         return associationSubnetId;
@@ -60,6 +78,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.associationSubnetId = associationSubnetId;
     }
 
+    /**
+     * Indicates whether the route table is the main route table for the VPC . Valid values are `` true `` or `` false``. Route tables that do not have an association ID are not returned in the response.
+     */
     @Filter("association.main")
     public String getAssociationMain() {
         return associationMain;
@@ -69,6 +90,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.associationMain = associationMain;
     }
 
+    /**
+     * The ID of the AWS account that owns the route table.
+     */
     public String getOwnerId() {
         return ownerId;
     }
@@ -77,6 +101,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.ownerId = ownerId;
     }
 
+    /**
+     * The ID of the route table.
+     */
     public String getRouteTableId() {
         return routeTableId;
     }
@@ -85,6 +112,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.routeTableId = routeTableId;
     }
 
+    /**
+     * The IPv4 CIDR range specified in a route in the table.
+     */
     @Filter("route.destination-cidr-block")
     public String getRouteDestinationCidrBlock() {
         return routeDestinationCidrBlock;
@@ -94,6 +124,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.routeDestinationCidrBlock = routeDestinationCidrBlock;
     }
 
+    /**
+     * The IPv6 CIDR range specified in a route in the route table.
+     */
     @Filter("route.destination-ipv6-cidr-block")
     public String getRouteDestinationIpv6CidrBlock() {
         return routeDestinationIpv6CidrBlock;
@@ -103,6 +136,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.routeDestinationIpv6CidrBlock = routeDestinationIpv6CidrBlock;
     }
 
+    /**
+     * The ID (prefix) of the AWS service specified in a route in the table.
+     */
     @Filter("route.destination-prefix-list-id")
     public String getRouteDestinationPrefixListId() {
         return routeDestinationPrefixListId;
@@ -112,6 +148,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.routeDestinationPrefixListId = routeDestinationPrefixListId;
     }
 
+    /**
+     * The ID of an egress-only Internet gateway specified in a route in the route table.
+     */
     @Filter("route.egress-only-internet-gateway-id")
     public String getRouteEgressOnlyInternetGatewayId() {
         return routeEgressOnlyInternetGatewayId;
@@ -121,6 +160,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.routeEgressOnlyInternetGatewayId = routeEgressOnlyInternetGatewayId;
     }
 
+    /**
+     * The ID of a gateway specified in a route in the table.
+     */
     @Filter("route.gateway-id")
     public String getRouteGatewayId() {
         return routeGatewayId;
@@ -130,6 +172,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.routeGatewayId = routeGatewayId;
     }
 
+    /**
+     * The ID of an instance specified in a route in the table.
+     */
     @Filter("route.instance-id")
     public String getRouteInstanceId() {
         return routeInstanceId;
@@ -139,6 +184,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.routeInstanceId = routeInstanceId;
     }
 
+    /**
+     * The ID of a NAT gateway.
+     */
     @Filter("route.nat-gateway-id")
     public String getRouteNatGatewayId() {
         return routeNatGatewayId;
@@ -148,6 +196,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.routeNatGatewayId = routeNatGatewayId;
     }
 
+    /**
+     * The ID of a transit gateway.
+     */
     @Filter("route.transit-gateway-id")
     public String getRouteTransitGatewayId() {
         return routeTransitGatewayId;
@@ -157,6 +208,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.routeTransitGatewayId = routeTransitGatewayId;
     }
 
+    /**
+     * Describes how the route was created. CreateRouteTable indicates that the route was automatically created when the route table was created; CreateRoute indicates that the route was manually added to the route table; EnableVgwRoutePropagation indicates that the route was propagated by route propagation.
+     */
     @Filter("route.origin")
     public String getRouteOrigin() {
         return routeOrigin;
@@ -166,6 +220,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.routeOrigin = routeOrigin;
     }
 
+    /**
+     * The state of a route in the route table (active | blackhole ). The blackhole state indicates that the route's target isn't available . Valid values are ``for example, the specified gateway isn't attached to the VPC, the specified NAT instance has been terminated, and so on``.
+     */
     @Filter("route.state")
     public String getRouteState() {
         return routeState;
@@ -175,6 +232,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.routeState = routeState;
     }
 
+    /**
+     * The ID of a VPC peering connection specified in a route in the table.
+     */
     @Filter("route.vpc-peering-connection-id")
     public String getRouteVpcPeeringConnectionId() {
         return routeVpcPeeringConnectionId;
@@ -184,7 +244,14 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.routeVpcPeeringConnectionId = routeVpcPeeringConnectionId;
     }
 
+    /**
+     * The key/value combination of a tag assigned to the resource.
+     */
     public Map<String, String> getTag() {
+        if (tag == null) {
+            tag = new HashMap<>();
+        }
+
         return tag;
     }
 
@@ -192,6 +259,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.tag = tag;
     }
 
+    /**
+     * The key of a tag assigned to the resource. Use this filter to find all resources assigned a tag with a specific key, regardless of the tag value.
+     */
     public String getTagKey() {
         return tagKey;
     }
@@ -200,6 +270,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.tagKey = tagKey;
     }
 
+    /**
+     * The ID of a transit gateway.
+     */
     public String getTransitGatewayId() {
         return transitGatewayId;
     }
@@ -208,6 +281,9 @@ public class RouteTableFinder extends AwsFinder<Ec2Client, RouteTable, RouteTabl
         this.transitGatewayId = transitGatewayId;
     }
 
+    /**
+     * The ID of the VPC for the route table.
+     */
     public String getVpcId() {
         return vpcId;
     }
