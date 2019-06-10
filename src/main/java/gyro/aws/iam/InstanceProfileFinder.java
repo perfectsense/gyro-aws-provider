9dedd087fec7217f0f8f43c90722387ecb2051f6
Renee Adkins <reneeskate@aol.com>
2019-06-10 11:43:06 -0400

    Implement instance profile finder

---
+++ src/main/java/gyro/aws/iam/InstanceProfileFinder.java
@@ -0,0 +1,58 @@
+package gyro.aws.iam;
+
+import gyro.aws.AwsFinder;
+import gyro.core.Type;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.iam.IamClient;
+import software.amazon.awssdk.services.iam.model.InstanceProfile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Query instance profiles.
+ *
+ * .. code-block:: gyro
+ *
+ *    instance-profile: $(aws::instance-profile EXTERNAL/* | name = '')
+ */
+@Type("instance-profile")
+public class InstanceProfileFinder extends AwsFinder<IamClient, InstanceProfile, InstanceProfileResource> {
+
+    private String name;
+
+    /**
+     * The name of the instance profile.
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected List<InstanceProfile> findAws(IamClient client, Map<String, String> filters) {
+        IamClient toUse = IamClient.builder()
+                .region(Region.AWS_GLOBAL)
+                .build();
+
+        List<InstanceProfile> instanceProfile = new ArrayList<>();
+
+        instanceProfile.add(toUse.getInstanceProfile(r -> r.instanceProfileName(filters.get("name"))).instanceProfile());
+
+        return instanceProfile;
+    }
+
+    @Override
+    protected List<InstanceProfile> findAllAws(IamClient client) {
+        IamClient toUse = IamClient.builder()
+                .region(Region.AWS_GLOBAL)
+                .build();
+
+        return toUse.listInstanceProfiles().instanceProfiles();
+    }
+}
