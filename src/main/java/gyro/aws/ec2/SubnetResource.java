84aefeb36cc2bfa6c836e190356d702e1563640c
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-06-13 14:01:25 -0400

    Update SubnetResource.java

--- src/main/java/gyro/aws/ec2/SubnetResource.java
+++ src/main/java/gyro/aws/ec2/SubnetResource.java
@@ -87,12 +87,12 @@ public class SubnetResource extends Ec2TaggableResource<Subnet> implements Copya
     }
 
     /**
-     * Assign a public IPv4 address to Network Interfaces created in this Subnet. Defaults to ``true``.
+     * Assign a public IPv4 address to Network Interfaces created in this Subnet. Defaults to ``false``.
      */
     @Updatable
     public Boolean getMapPublicIpOnLaunch() {
         if (mapPublicIpOnLaunch == null) {
-            mapPublicIpOnLaunch = true;
+            mapPublicIpOnLaunch = false;
         }
 
         return mapPublicIpOnLaunch;
