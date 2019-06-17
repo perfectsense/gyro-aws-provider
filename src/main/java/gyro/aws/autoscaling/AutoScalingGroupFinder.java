4bdfe6f6232907856099b4fd501183eb046b9e0c
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-06-17 15:41:12 -0400

    Refactor auto scaling group finder.

--- src/main/java/gyro/aws/autoscaling/AutoScalingGroupFinder.java
+++ src/main/java/gyro/aws/autoscaling/AutoScalingGroupFinder.java
@@ -6,6 +6,8 @@ import gyro.core.Type;
 import software.amazon.awssdk.services.autoscaling.AutoScalingClient;
 import software.amazon.awssdk.services.autoscaling.model.AutoScalingException;
 import software.amazon.awssdk.services.autoscaling.model.AutoScalingGroup;
+import software.amazon.awssdk.services.autoscaling.model.DescribeAutoScalingGroupsRequest;
+import software.amazon.awssdk.services.autoscaling.model.DescribeAutoScalingGroupsResponse;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -24,7 +26,7 @@ public class AutoScalingGroupFinder extends AwsFinder<AutoScalingClient, AutoSca
     private String autoScalingGroupName;
 
     /**
-     * The name of the auto scaling group.
+     * The name of the Auto Scaling Group.
      */
     public String getAutoScalingGroupName() {
         return autoScalingGroupName;
@@ -36,21 +38,53 @@ public class AutoScalingGroupFinder extends AwsFinder<AutoScalingClient, AutoSca
 
     @Override
     protected List<AutoScalingGroup> findAllAws(AutoScalingClient client) {
-        return client.describeAutoScalingGroups().autoScalingGroups();
+        List<AutoScalingGroup> autoScalingGroups = new ArrayList<>();
+
+        String marker = null;
+        DescribeAutoScalingGroupsResponse response;
+        do {
+            if (ObjectUtils.isBlank(marker)) {
+                response = client.describeAutoScalingGroups();
+            } else {
+                response = client.describeAutoScalingGroups(DescribeAutoScalingGroupsRequest.builder().nextToken(marker).build());
+            }
+
+            marker = response.nextToken();
+            autoScalingGroups.addAll(response.autoScalingGroups());
+
+        } while (!ObjectUtils.isBlank(marker));
+
+        return autoScalingGroups;
     }
 
     @Override
     protected List<AutoScalingGroup> findAws(AutoScalingClient client, Map<String, String> filters) {
         List<AutoScalingGroup> autoScalingGroups = new ArrayList<>();
 
+        String marker = null;
+        DescribeAutoScalingGroupsResponse response;
         if (filters.containsKey("auto-scaling-group-name") && !ObjectUtils.isBlank(filters.get("auto-scaling-group-name"))) {
-
             try {
-                autoScalingGroups.addAll(
-                    client.describeAutoScalingGroups(
-                        r -> r.autoScalingGroupNames(Collections.singleton(filters.get("auto-scaling-group-name")))
-                    ).autoScalingGroups()
-                );
+                do {
+                    if (ObjectUtils.isBlank(marker)) {
+                        response = client.describeAutoScalingGroups(
+                            DescribeAutoScalingGroupsRequest.builder()
+                                .autoScalingGroupNames(Collections.singleton(filters.get("auto-scaling-group-name")))
+                                .build()
+                        );
+                    } else {
+                        response = client.describeAutoScalingGroups(
+                            DescribeAutoScalingGroupsRequest.builder()
+                                .autoScalingGroupNames(Collections.singleton(filters.get("auto-scaling-group-name")))
+                                .nextToken(marker)
+                                .build()
+                        );
+                    }
+
+                    marker = response.nextToken();
+                    autoScalingGroups.addAll(response.autoScalingGroups());
+
+                } while (!ObjectUtils.isBlank(marker));
             } catch (AutoScalingException ex) {
                 if (!ex.getLocalizedMessage().contains("does not exist")) {
                     throw ex;
