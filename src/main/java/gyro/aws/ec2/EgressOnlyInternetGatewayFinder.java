1ac517e9040ec9a002145c7e7d0ac06beed028a9
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-06-20 14:59:56 -0400

    Add pagination to egree only internet gareway finder.

--- src/main/java/gyro/aws/ec2/EgressOnlyInternetGatewayFinder.java
+++ src/main/java/gyro/aws/ec2/EgressOnlyInternetGatewayFinder.java
@@ -1,10 +1,14 @@
 package gyro.aws.ec2;
 
+import com.psddev.dari.util.ObjectUtils;
 import gyro.aws.AwsFinder;
 import gyro.core.Type;
 import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.model.DescribeEgressOnlyInternetGatewaysRequest;
+import software.amazon.awssdk.services.ec2.model.DescribeEgressOnlyInternetGatewaysResponse;
 import software.amazon.awssdk.services.ec2.model.EgressOnlyInternetGateway;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -33,13 +37,39 @@ public class EgressOnlyInternetGatewayFinder extends AwsFinder<Ec2Client, Egress
 
     @Override
     protected List<EgressOnlyInternetGateway> findAllAws(Ec2Client client) {
-        return client.describeEgressOnlyInternetGateways().egressOnlyInternetGateways();
+        return getEgressOnlyInternetGateways(client, null);
     }
 
     @Override
     protected List<EgressOnlyInternetGateway> findAws(Ec2Client client, Map<String, String> filters) {
-        return client.describeEgressOnlyInternetGateways(
-            r -> r.egressOnlyInternetGatewayIds(Collections.singleton(filters.get("egress-only-internet-gateway-id")))
-        ).egressOnlyInternetGateways();
+        return getEgressOnlyInternetGateways(client, filters);
+    }
+
+    private List<EgressOnlyInternetGateway> getEgressOnlyInternetGateways(Ec2Client client, Map<String, String> filters) {
+        List<EgressOnlyInternetGateway> egressOnlyInternetGateways = new ArrayList<>();
+
+        DescribeEgressOnlyInternetGatewaysRequest.Builder builder = DescribeEgressOnlyInternetGatewaysRequest.builder();
+
+        if (filters != null) {
+            if (filters.containsKey("egress-only-internet-gateway-id")) {
+                builder = builder.egressOnlyInternetGatewayIds(Collections.singleton(filters.get("egress-only-internet-gateway-id")));
+            }
+        }
+
+        String marker = null;
+        DescribeEgressOnlyInternetGatewaysResponse response;
+
+        do {
+            if (ObjectUtils.isBlank(marker)) {
+                response = client.describeEgressOnlyInternetGateways(builder.build());
+            } else {
+                response = client.describeEgressOnlyInternetGateways(builder.nextToken(marker).build());
+            }
+
+            marker = response.nextToken();
+            egressOnlyInternetGateways.addAll(response.egressOnlyInternetGateways());
+        } while (!ObjectUtils.isBlank(marker));
+
+        return egressOnlyInternetGateways;
     }
 }
