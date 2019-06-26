e5fab9f82ba57d94d9dcada7b1172ef1d50079eb
Hao Sun <PSDhao@users.noreply.github.com>
2019-06-26 14:26:52 -0400

    Remove unused imports

--- src/main/java/gyro/aws/ec2/SecurityGroupIngressRuleResource.java
+++ src/main/java/gyro/aws/ec2/SecurityGroupIngressRuleResource.java
@@ -3,7 +3,6 @@ package gyro.aws.ec2;
 import gyro.core.resource.Resource;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 import software.amazon.awssdk.services.ec2.model.Ec2Exception;
-import software.amazon.awssdk.services.ec2.model.IpPermission;
 
 import java.util.Set;
 
