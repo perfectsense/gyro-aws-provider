d67ecea905c2c27d00267add7733fca94f511c22
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-07-03 09:57:59 -0400

    Update egress only internet gateway finder to use paginator api.

--- src/main/java/gyro/aws/ec2/EgressOnlyInternetGatewayFinder.java
+++ src/main/java/gyro/aws/ec2/EgressOnlyInternetGatewayFinder.java
@@ -1,16 +1,14 @@
 package gyro.aws.ec2;
 
-import com.psddev.dari.util.ObjectUtils;
 import gyro.aws.AwsFinder;
 import gyro.core.Type;
 import software.amazon.awssdk.services.ec2.Ec2Client;
-import software.amazon.awssdk.services.ec2.model.DescribeEgressOnlyInternetGatewaysRequest;
-import software.amazon.awssdk.services.ec2.model.DescribeEgressOnlyInternetGatewaysResponse;
 import software.amazon.awssdk.services.ec2.model.EgressOnlyInternetGateway;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Query egress only internet gateway.
@@ -36,39 +34,16 @@ public class EgressOnlyInternetGatewayFinder extends AwsFinder<Ec2Client, Egress
 
     @Override
     protected List<EgressOnlyInternetGateway> findAllAws(Ec2Client client) {
-        return getEgressOnlyInternetGateways(client, null);
+        return client.describeEgressOnlyInternetGatewaysPaginator().egressOnlyInternetGateways().stream().collect(Collectors.toList());
     }
 
     @Override
     protected List<EgressOnlyInternetGateway> findAws(Ec2Client client, Map<String, String> filters) {
-        return getEgressOnlyInternetGateways(client, filters);
-    }
+        if (filters.containsKey("egress-only-internet-gateway-id")) {
+            return client.describeEgressOnlyInternetGatewaysPaginator(r -> r.egressOnlyInternetGatewayIds(filters.get("egress-only-internet-gateway-id"))).egressOnlyInternetGateways().stream().collect(Collectors.toList());
 
-    private List<EgressOnlyInternetGateway> getEgressOnlyInternetGateways(Ec2Client client, Map<String, String> filters) {
-        List<EgressOnlyInternetGateway> egressOnlyInternetGateways = new ArrayList<>();
-
-        DescribeEgressOnlyInternetGatewaysRequest.Builder builder = DescribeEgressOnlyInternetGatewaysRequest.builder();
-
-        if (filters != null) {
-            if (filters.containsKey("egress-only-internet-gateway-id")) {
-                builder = builder.egressOnlyInternetGatewayIds(filters.get("egress-only-internet-gateway-id"));
-            }
+        } else {
+            return Collections.emptyList();
         }
-
-        String marker = null;
-        DescribeEgressOnlyInternetGatewaysResponse response;
-
-        do {
-            if (ObjectUtils.isBlank(marker)) {
-                response = client.describeEgressOnlyInternetGateways(builder.build());
-            } else {
-                response = client.describeEgressOnlyInternetGateways(builder.nextToken(marker).build());
-            }
-
-            marker = response.nextToken();
-            egressOnlyInternetGateways.addAll(response.egressOnlyInternetGateways());
-        } while (!ObjectUtils.isBlank(marker));
-
-        return egressOnlyInternetGateways;
     }
 }
