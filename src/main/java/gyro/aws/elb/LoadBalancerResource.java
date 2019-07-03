420f5a6e37c300fa6c4bc6f9547157d95089f9e0
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-07-03 14:56:44 -0400

    Check if load balancer exist before create.

--- src/main/java/gyro/aws/elb/LoadBalancerResource.java
+++ src/main/java/gyro/aws/elb/LoadBalancerResource.java
@@ -5,6 +5,7 @@ import gyro.aws.Copyable;
 import gyro.aws.ec2.InstanceResource;
 import gyro.aws.ec2.SecurityGroupResource;
 import gyro.aws.ec2.SubnetResource;
+import gyro.core.GyroException;
 import gyro.core.Type;
 import gyro.core.resource.Id;
 import gyro.core.resource.Output;
@@ -181,6 +182,10 @@ public class LoadBalancerResource extends AwsResource implements Copyable<LoadBa
     @Override
     public void create() {
         ElasticLoadBalancingClient client = createClient(ElasticLoadBalancingClient.class);
+
+        if (getLoadBalancer(client) != null) {
+            throw new GyroException("A load balancer with the same name exist.");
+        }
 
         CreateLoadBalancerResponse response = client.createLoadBalancer(r -> r.listeners(toListeners())
                 .securityGroups(getSecurityGroups().stream().map(SecurityGroupResource::getGroupId).collect(Collectors.toList()))
