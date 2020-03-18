3b1e77a105ea0c79c1d084f0befd89e01e00e184
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2020-03-18 15:22:37 -0400

    Add finder for security group rules

---
+++ src/main/java/gyro/aws/ec2/SecurityGroupRulesFinder.java
@@ -0,0 +1,50 @@
+package gyro.aws.ec2;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import gyro.aws.AwsFinder;
+import gyro.core.Type;
+import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.model.SecurityGroup;
+
+@Type("security-group-rules")
+public class SecurityGroupRulesFinder extends AwsFinder<Ec2Client, SecurityGroup, SecurityGroupRulesResource> {
+
+    private String groupId;
+    private String groupName;
+
+    /**
+     * The ID of the security group.
+     */
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    /**
+     * The name of the security group.
+     */
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    @Override
+    protected List<SecurityGroup> findAws(Ec2Client client, Map<String, String> filters) {
+        return client.describeSecurityGroupsPaginator(r -> r.filters(createFilters(filters))).securityGroups().stream().collect(
+            Collectors.toList());
+    }
+
+    @Override
+    protected List<SecurityGroup> findAllAws(Ec2Client client) {
+        return client.describeSecurityGroupsPaginator().securityGroups().stream().collect(Collectors.toList());
+    }
+}
