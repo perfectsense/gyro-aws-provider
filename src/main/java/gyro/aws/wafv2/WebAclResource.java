ae21b3cc12efe82a47e704f754a67a9896ed00e6
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2020-07-15 16:57:53 -0400

    Add license snippet

--- src/main/java/gyro/aws/wafv2/WebAclResource.java
+++ src/main/java/gyro/aws/wafv2/WebAclResource.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020, Perfect Sense, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package gyro.aws.wafv2;
 
 import java.util.ArrayList;
@@ -31,7 +47,7 @@ import software.amazon.awssdk.services.wafv2.model.ResourceType;
 import software.amazon.awssdk.services.wafv2.model.WafNonexistentItemException;
 import software.amazon.awssdk.services.wafv2.model.WebACL;
 
-@Type("waf-web-acl")
+@Type("wafv2-web-acl")
 public class WebAclResource extends WafTaggableResource implements Copyable<WebACL> {
 
     private String name;
