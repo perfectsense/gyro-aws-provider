cab6c6a66413e186f3aaf88a25b009c4271a5802
iHaoo <kennytran95@gmail.com>
2020-08-05 18:27:04 -0400

    Updated findAllAws to use paginator and stream

--- src/main/java/gyro/aws/secrets/SecretFinder.java
+++ src/main/java/gyro/aws/secrets/SecretFinder.java
@@ -1,44 +1,38 @@
 package gyro.aws.secrets;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import gyro.aws.AwsFinder;
 import gyro.core.Type;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.DescribeSecretResponse;
-import software.amazon.awssdk.services.secretsmanager.model.SecretListEntry;
 
 @Type("secret")
 public class SecretFinder extends AwsFinder<SecretsManagerClient, DescribeSecretResponse, SecretResource> {
 
     @Override
     protected List<DescribeSecretResponse> findAllAws(SecretsManagerClient client) {
-        List<DescribeSecretResponse> responseList = new ArrayList<>();
-
-        for (SecretListEntry entry : client.listSecrets().secretList()) {
-            DescribeSecretResponse response = DescribeSecretResponse.builder()
-                .arn(entry.arn())
-                .deletedDate(entry.deletedDate())
-                .description(entry.description())
-                .kmsKeyId(entry.kmsKeyId())
-                .lastAccessedDate(entry.lastAccessedDate())
-                .lastChangedDate(entry.lastChangedDate())
-                .lastRotatedDate(entry.lastRotatedDate())
-                .name(entry.name())
-                .owningService(entry.owningService())
-                .rotationEnabled(entry.rotationEnabled())
-                .rotationLambdaARN(entry.rotationLambdaARN())
-                .rotationRules(entry.rotationRules())
-                .tags(entry.tags())
-                .versionIdsToStages(entry.secretVersionsToStages())
-                .build();
-
-            responseList.add(response);
-        }
-
-        return responseList;
+        return client.listSecretsPaginator().stream().flatMap(list ->
+            list.secretList().stream().map(entry -> {
+                return DescribeSecretResponse.builder()
+                    .arn(entry.arn())
+                    .deletedDate(entry.deletedDate())
+                    .description(entry.description())
+                    .kmsKeyId(entry.kmsKeyId())
+                    .lastAccessedDate(entry.lastAccessedDate())
+                    .lastChangedDate(entry.lastChangedDate())
+                    .lastRotatedDate(entry.lastRotatedDate())
+                    .name(entry.name())
+                    .owningService(entry.owningService())
+                    .rotationEnabled(entry.rotationEnabled())
+                    .rotationLambdaARN(entry.rotationLambdaARN())
+                    .rotationRules(entry.rotationRules())
+                    .tags(entry.tags())
+                    .versionIdsToStages(entry.secretVersionsToStages())
+                    .build();
+            })).collect(Collectors.toList());
     }
 
     @Override
