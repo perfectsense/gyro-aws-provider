5ffef90cb8b85f5a265d7f5264e249be6b606d16
iHaoo <kennytran95@gmail.com>
2020-08-05 17:40:47 -0400

    Updated finder to convert SecretListEntry to DescribeSecretResponse list

--- src/main/java/gyro/aws/secrets/SecretFinder.java
+++ src/main/java/gyro/aws/secrets/SecretFinder.java
@@ -1,23 +1,37 @@
 package gyro.aws.secrets;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import gyro.aws.AwsFinder;
 import gyro.core.Type;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.DescribeSecretResponse;
 import software.amazon.awssdk.services.secretsmanager.model.SecretListEntry;
 
 @Type("secret")
-public class SecretFinder extends AwsFinder<SecretsManagerClient, SecretListEntry, SecretResource> {
+public class SecretFinder extends AwsFinder<SecretsManagerClient, DescribeSecretResponse, SecretResource> {
 
     @Override
-    protected List<SecretListEntry> findAllAws(SecretsManagerClient client) {
-        return client.listSecrets().secretList();
+    protected List<DescribeSecretResponse> findAllAws(SecretsManagerClient client) {
+        List<DescribeSecretResponse> responseList = new ArrayList<>();
+
+        for (SecretListEntry entry : client.listSecrets().secretList()) {
+            DescribeSecretResponse response = DescribeSecretResponse.builder()
+                .arn(entry.arn())
+                .name(entry.name())
+                .description(entry.description())
+                .build();
+
+            responseList.add(response);
+        }
+
+        return responseList;
     }
 
     @Override
-    protected List<SecretListEntry> findAws(
+    protected List<DescribeSecretResponse> findAws(
         SecretsManagerClient client, Map<String, String> filters) {
         return null;
     }
