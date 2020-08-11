5a022804d29c43bbb775d1ff3371f850963978ad
iHaoo <kennytran95@gmail.com>
2020-08-11 18:20:49 -0400

    Updated licenses

--- src/main/java/gyro/aws/secretsmanager/package-info.java
+++ src/main/java/gyro/aws/secretsmanager/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020, Brightspot, Inc.
+ * Copyright 2020, Perfect Sense, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
