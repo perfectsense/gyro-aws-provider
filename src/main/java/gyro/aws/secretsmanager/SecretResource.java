3620b4ceaff8f2174a7fabc42624f891feb43ab3
iHaoo <kennytran95@gmail.com>
2020-08-24 14:04:33 -0400

    Added condition for update for non-tag fields changed

--- src/main/java/gyro/aws/secretsmanager/SecretResource.java
+++ src/main/java/gyro/aws/secretsmanager/SecretResource.java
@@ -362,7 +362,7 @@ public class SecretResource extends AwsResource implements Copyable<DescribeSecr
         GyroUI ui, State state, Resource current, Set<String> changedFieldNames) throws Exception {
         SecretsManagerClient client = createClient(SecretsManagerClient.class);
 
-        if (changedFieldNames.size() >= 2) {
+        if (changedFieldNames.size() >= 1 || !changedFieldNames.contains("tags")) {
             client.updateSecret(r -> r.secretId(getArn())
                 .description(getDescription())
                 .kmsKeyId(getKmsKey() != null ? getKmsKey().getArn() : null)
