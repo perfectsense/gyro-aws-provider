8f059255f46247ce362a6fed2cdcd872b182fc40
Harshil Jain <harjain99@gmail.com>
2021-01-21 10:26:06 -0500

    Update license

--- src/main/java/gyro/aws/eks/EksAddonResource.java
+++ src/main/java/gyro/aws/eks/EksAddonResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021, Perfect Sense, Inc.
+ * Copyright 2021, Brightspot.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
