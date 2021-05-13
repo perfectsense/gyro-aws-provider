0a51e3c7ddbb8379af9f768748de97265dec36eb
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2021-05-13 17:35:49 -0400

    Convert eks authentication into a subresource

--- src/main/java/gyro/aws/eks/EksAuthentication.java
+++ src/main/java/gyro/aws/eks/EksAuthentication.java
@@ -24,7 +24,6 @@ import java.util.concurrent.TimeUnit;
 import gyro.aws.AwsResource;
 import gyro.aws.Copyable;
 import gyro.core.GyroUI;
-import gyro.core.Type;
 import gyro.core.Wait;
 import gyro.core.resource.Id;
 import gyro.core.resource.Resource;
@@ -45,48 +44,38 @@ import software.amazon.awssdk.services.eks.model.ResourceNotFoundException;
  *
  * .. code-block:: gyro
  *
- *     aws::eks-authentication eks-authentication-example
- *         cluster: "cluster-example"
- *         name: "onelogin"
- *         type: "oidc"
+ *     aws::eks-cluster eks-authentication-example
+ *         .
+ *         .
+ *         .
+ *         authentication
+ *              name: "onelogin"
+ *              type: "oidc"
  *
- *         config
- *             client-id: "valid client id"
- *             groups-claim: "groups"
- *             groups-prefix: "onelogin-group:"
- *             config-name: "onelogin"
- *             issuer-url: "valid issuer url"
- *             username-prefix: "onelogin-user:"
- *         end
+ *              config
+ *                  client-id: "valid client id"
+ *                  groups-claim: "groups"
+ *                  groups-prefix: "onelogin-group:"
+ *                  config-name: "onelogin"
+ *                  issuer-url: "valid issuer url"
+ *                  username-prefix: "onelogin-user:"
+ *              end
  *
- *         tags: {
- *             Name: "eks-authentication-example"
- *         }
+ *              tags: {
+ *                  Name: "eks-authentication-example"
+ *              }
+ *          end
  *     end
  */
-@Type("eks-authentication")
 public class EksAuthentication extends AwsResource implements Copyable<IdentityProviderConfigResponse> {
 
-    private EksClusterResource cluster;
     private String name;
     private String type;
     private OidcIdentityProviderConfiguration config;
     private Map<String, String> tags;
 
     /**
-     * The cluster to associate the identity provider with.
-     */
-    @Required
-    public EksClusterResource getCluster() {
-        return cluster;
-    }
-
-    public void setCluster(EksClusterResource cluster) {
-        this.cluster = cluster;
-    }
-
-    /**
-     * The name of the identity provider configuration
+     * The name of the identity provider configuration.
      */
     @Required
     @Id
@@ -143,7 +132,6 @@ public class EksAuthentication extends AwsResource implements Copyable<IdentityP
     @Override
     public void copyFrom(IdentityProviderConfigResponse model) {
         setTags(model.oidc().tags());
-        setCluster(findById(EksClusterResource.class, model.oidc().clusterName()));
 
         String arn = model.oidc().identityProviderConfigArn();
         String[] split = arn.split("/");
@@ -157,15 +145,6 @@ public class EksAuthentication extends AwsResource implements Copyable<IdentityP
 
     @Override
     public boolean refresh() {
-        EksClient client = createClient(EksClient.class);
-
-        IdentityProviderConfigResponse response = getIdentityProviderConfigResponse(client);
-
-        if (response == null) {
-            return false;
-        }
-
-        copyFrom(response);
         return true;
     }
 
@@ -173,13 +152,17 @@ public class EksAuthentication extends AwsResource implements Copyable<IdentityP
     public void create(GyroUI ui, State state) throws Exception {
         EksClient client = createClient(EksClient.class);
 
-        client.associateIdentityProviderConfig(r -> r.clusterName(getCluster().getName()).oidc(getConfig().toOidcIdentityProviderConfig()).tags(getTags()));
+        EksClusterResource parent = (EksClusterResource) parent();
+
+        client.associateIdentityProviderConfig(r -> r.clusterName(parent.getName())
+            .oidc(getConfig().toOidcIdentityProviderConfig())
+            .tags(getTags()));
 
         Wait.atMost(30, TimeUnit.MINUTES)
             .checkEvery(2, TimeUnit.MINUTES)
             .prompt(false)
             .until(() -> {
-                IdentityProviderConfigResponse response = getIdentityProviderConfigResponse(client);
+                IdentityProviderConfigResponse response = getIdentityProviderConfigResponse(client, parent.getName(), getName(), getType());
                 return response != null && response.oidc().status() == ConfigStatus.ACTIVE;
             });
     }
@@ -194,20 +177,23 @@ public class EksAuthentication extends AwsResource implements Copyable<IdentityP
     public void delete(GyroUI ui, State state) throws Exception {
         EksClient client = createClient(EksClient.class);
 
-        client.disassociateIdentityProviderConfig(r -> r.clusterName(getCluster().getName()).identityProviderConfig(i -> i.name(getName()).type(getType())));
+        EksClusterResource parent = (EksClusterResource) parent();
+
+        client.disassociateIdentityProviderConfig(r -> r.clusterName(parent.getName())
+            .identityProviderConfig(i -> i.name(getName()).type(getType())));
 
         Wait.atMost(30, TimeUnit.MINUTES)
             .checkEvery(2, TimeUnit.MINUTES)
             .prompt(false)
-            .until(() -> getIdentityProviderConfigResponse(client) == null);
+            .until(() -> getIdentityProviderConfigResponse(client, parent.getName(), getName(), getType()) == null);
     }
 
-    private IdentityProviderConfigResponse getIdentityProviderConfigResponse(EksClient client) {
+    protected static IdentityProviderConfigResponse getIdentityProviderConfigResponse(EksClient client, String clusterName, String name, String type) {
         IdentityProviderConfigResponse identityProviderConfigResponse = null;
         try {
             DescribeIdentityProviderConfigResponse response = client.describeIdentityProviderConfig(
-                r -> r.clusterName(getCluster().getName())
-                    .identityProviderConfig(i -> i.name(getName()).type(getType())));
+                r -> r.clusterName(clusterName)
+                    .identityProviderConfig(i -> i.name(name).type(type)));
 
             identityProviderConfigResponse = response.identityProviderConfig();
 
