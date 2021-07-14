6eef86d33532ae9c2d5f30dcebe9ec11fd054381
Harshil Jain <harjain99@gmail.com>
2021-07-14 16:02:26 -0400

    Update validation string

--- src/main/java/gyro/aws/ec2/InstanceResource.java
+++ src/main/java/gyro/aws/ec2/InstanceResource.java
@@ -803,7 +803,7 @@ public class InstanceResource extends Ec2TaggableResource<Instance> implements G
         }
 
         if ((getLaunchTemplate() == null && ObjectUtils.isBlank(getInstanceType()))) {
-            errors.add(new ValidationError(this, "instance-type", "The 'instance-type' is required if a 'launch-template' is not specified."));
+            errors.add(new ValidationError(this, "instance-type", "'instance-type' is required if 'launch-template' is not specified."));
         }
 
         if (!getCapacityReservation().equalsIgnoreCase("none")
