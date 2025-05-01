55f2d68ec99944bf215c3fcedf0adef759d4e4ec
Tommy Gonzalez <tommy.gonzalez@brightspot.com>
2025-05-01 11:39:49 -0400

    updated domain name to AWS ARN according to the documentation

--- src/main/java/gyro/aws/opensearch/OpenSearchOutboundConnectionResource.java
+++ src/main/java/gyro/aws/opensearch/OpenSearchOutboundConnectionResource.java
@@ -230,10 +230,10 @@ public class OpenSearchOutboundConnectionResource extends AwsResource implements
             r -> r.connectionAlias(getConnectionAlias())
                 .connectionMode(getConnectionMode())
                 .localDomainInfo(s -> s.awsDomainInformation(
-                    t -> t.domainName(getLocalDomain().getDomainName())
+                    t -> t.domainName(getLocalDomain().getArn())
                         .ownerId(localOwnerId)))
                 .remoteDomainInfo(s -> s.awsDomainInformation(
-                    t -> t.domainName(getRemoteDomain().getDomainName())
+                    t -> t.domainName(getRemoteDomain().getArn())
                         .ownerId(remoteOwnerId)))
         );
 
